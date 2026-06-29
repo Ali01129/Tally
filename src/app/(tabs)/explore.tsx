7f28a9cd7365f7b0276711b889c668e107b23b49
ali01129 <chaudharyalinawazz@gmail.com>
2026-06-30 03:32:47 +0500

    Fixed the bottom navbar

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -3,9 +3,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabTwoScreen() {
   return (
-    <View className="flex-1 bg-black">
+    <View className="flex-1 bg-white">
       <SafeAreaView className="flex-1 px-4 pt-10">
-        <Text className="text-2xl font-bold text-white">Explore</Text>
+        <Text className="text-2xl font-bold text-black">Explore</Text>
       </SafeAreaView>
     </View>
   );
--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -1,30 +1,28 @@
-import { NativeTabs } from 'expo-router/unstable-native-tabs';
-import { useColorScheme } from 'react-native';
-
-import { Colors } from '@/constants/theme';
+import { Colors } from "@/constants/theme";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function AppTabs() {
-  const scheme = useColorScheme();
-  const colors = Colors[scheme === 'unspecified' ? 'light' : scheme];
+  const tabBarColors = Colors.light;
 
   return (
     <NativeTabs
-      backgroundColor={colors.background}
-      indicatorColor={colors.backgroundElement}
-      labelStyle={{ selected: { color: colors.text } }}>
+      backgroundColor={tabBarColors.background}
+      indicatorColor={tabBarColors.backgroundElement}
+      labelStyle={{ selected: { color: tabBarColors.text } }}
+    >
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/home.png')}
-          renderingMode="template"
+          sf={{ default: "house", selected: "house.fill" }}
+          md={{ default: "home_filled", selected: "home" }}
         />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="explore">
         <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
+          sf={{ default: "magnifyingglass", selected: "magnifyingglass" }}
+          md={{ default: "search", selected: "search" }}
         />
       </NativeTabs.Trigger>
     </NativeTabs>
